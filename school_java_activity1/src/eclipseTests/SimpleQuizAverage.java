a7eba7a09fefe990f0d2627a906aac2e90d54cfc
IanDG39 <bigjopoy@gmail.com>
2020-03-27 17:31:41 +0800

    fixed it, i think

--- school_java_activity1/src/eclipseTests/SimpleQuizAverage.java
+++
@@ -1,5 +0,0 @@
-package eclipseTests;
-
-public class SimpleQuizAverage {
-
-}
---
+++ src/school_java_activity1/SimpleQuizAverage.java
@@ -0,0 +1,28 @@
+package school_java_activity1;
+
+public class SimpleQuizAverage 
+{
+	private double score[];
+	private double average;
+	
+	public SimpleQuizAverage()
+	{
+		this.score = new double[4];
+	}
+	public SimpleQuizAverage(int arrayLength)
+	{
+		this.score = new double[arrayLength];
+	}
+	
+	public void setScore(int index, double value)
+	{
+		this.score[index] = value;
+	}
+	
+	public double getAverage()
+	{
+		
+		
+		return this.average;
+	}
+}
