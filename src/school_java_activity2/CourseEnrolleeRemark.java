8914c708f56aef34cb520da3e4db8cfb51b8c577
IanDG39 <bigjopoy@gmail.com>
2020-04-03 22:45:57 +0800

    act 2, items 1-4 done

---
+++ src/school_java_activity2/CourseEnrolleeRemark.java
@@ -0,0 +1,61 @@
+package school_java_activity2;
+
+public class CourseEnrolleeRemark 
+{
+	private int enrolleeCount;
+	private String courseTitle;
+	
+	private String[] remarks = new String[] 
+	{
+		"Dissolved", "Open", "Closed", "Invalid"
+	};
+	private int[] remarkRange = new int[]
+	{
+		3,9,10
+	};
+	
+	public CourseEnrolleeRemark() {}
+	
+	public void setEnrolleeCount(int enrolleeCount)
+	{
+		this.enrolleeCount = enrolleeCount;
+	}
+	
+	public void setCourseTitle(String courseTitle)
+	{
+		this.courseTitle = courseTitle;
+	}
+	
+	public int getEnrolleeCount()
+	{
+		return this.enrolleeCount;
+	}
+	
+	public String getCourseTitle()
+	{
+		return this.courseTitle;
+	}
+	
+	public String getRemark()
+	{
+		boolean belowEleven = false;
+		String output = "";
+		
+		for(int i = 0; i < remarkRange.length; i++)
+		{
+			if(this.enrolleeCount <= remarkRange[i])
+			{
+				output = remarks[i];
+				belowEleven = true;
+				break;
+			}
+		}
+		
+		if(!belowEleven) 
+		{
+			output = remarks[remarks.length - 1];
+		}
+		
+		return output;
+	}
+}
